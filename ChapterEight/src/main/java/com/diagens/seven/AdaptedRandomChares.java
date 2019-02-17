000192842d12bb191b3e3bedebb1d95cc4a0b27f
sanmao <sanmao@qq.com>
2019-02-17 23:11:05 +0800

    ChapterNine 216PageNum

---
+++ ChapterEight/src/main/java/com/diagens/seven/AdaptedRandomChares.java
@@ -0,0 +1,33 @@
+package com.diagens.seven;
+
+import java.io.IOException;
+import java.nio.CharBuffer;
+import java.util.Scanner;
+
+/**
+ * @author ZNJ
+ * @create 2019/2/17-23:04
+ */
+public class AdaptedRandomChares extends RandomChares implements Readable {
+    private int count;
+
+    public AdaptedRandomChares(int count) {
+        this.count = count;
+    }
+
+    @Override
+    public int read(CharBuffer cb) throws IOException {
+        if(count--==0){
+           return -1;
+        }
+        String result=next()+" ";
+        cb.append(result);
+        return result.length();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(new AdaptedRandomChares(7));
+        while (scanner.hasNext())
+            System.out.println(scanner.next());
+    }
+}
---
+++ ChapterEight/src/main/java/com/diagens/seven/RandomChares.java
@@ -0,0 +1,14 @@
+package com.diagens.seven;
+
+import java.util.Random;
+
+/**
+ * @author ZNJ
+ * @create 2019/2/17-23:01
+ */
+public class RandomChares {
+    private static Random random=new Random();
+    public char next(){
+        return (char)('a'+random.nextInt(26));
+    }
+}
