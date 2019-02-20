7a2744fd7b3ff1ee3d6bbc7206658df3625b86ed
sanmao <sanmao@qq.com>
2019-02-20 21:43:15 +0800

    ChapterNine 229PageNum

--- ChapterNine/src/main/java/com/diagens/five/Destination.java
+++ ChapterNine/src/main/java/com/diagens/five/Destination.java
@@ -2,8 +2,8 @@ package com.diagens.five;
 
 /**
  * @author ZNJ
- * @create 2019/2/19-23:09
+ * @create 2019/2/19-23:41
  */
 public interface Destination {
-    String readLabel();
+     String readLabel();
 }
